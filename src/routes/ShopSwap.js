fcf35d40233577875cbfcee284dde6ad77da6b31
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-11-19 14:51:35 -0700

    Added shop options html

--- src/routes/ShopSwap.js
+++ src/routes/ShopSwap.js
@@ -6,30 +6,84 @@ export default function ShopSwap() {
   return (
     <div className="ShopSwap container-fluid">
       <div className="background-img">
-        <h2>Shop N Swap</h2>
+        <h2>Shop: Shop N Swap</h2>
       </div>
-      <div className="row">
-        <div className="col-lg-3">
+      <div className="row shop-glasses">
+        <div className="col-lg-6">
           <img src={ShopGlasses} alt="Glasses" className="img-fluid"></img>
         </div>
-        <div className="col-lg-3">
-          <div>Sphere</div>
-          <div>Left:-1.00</div>
-          <div>Right:+1.00</div>
-        </div>
-        <div className="col-lg-3">
-          <div>IPD</div>
-          <div>Left: 32.0</div>
-          <div>Right: 32.0</div>
-        </div>
-        <div className="col-lg-3">
-          <div>Frame</div>
-          <div>Bridge: 22</div>
-          <div>Lens: 49</div>
-          <div>Temple: 150</div>
+        <div className="col-lg-6 glasses-detail">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>Sphere</div>
+              <div>Left:-1.00</div>
+              <div>Right:+1.00</div>
+            </div>
+            <div className="col-lg-4">
+              <div>IPD</div>
+              <div>Left: 32.0</div>
+              <div>Right: 32.0</div>
+            </div>
+            <div className="col-lg-4">
+              <div>Frame</div>
+              <div>Bridge: 22</div>
+              <div>Lens: 49</div>
+              <div>Temple: 150</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <div>Distance</div>
+              <div>Left:-1.00</div>
+            </div>
+            <div className="col-lg-4">
+              <div>Price</div>
+              <div>$9.99</div>
+            </div>
+            <div className="col-lg-4 mt-3">
+              <button>View</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="row "></div>
+      <div className="row shop-glasses">
+        <div className="col-lg-6">
+          <img src={ShopGlasses} alt="Glasses" className="img-fluid"></img>
+        </div>
+        <div className="col-lg-6 glasses-detail">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>Sphere</div>
+              <div>Left:-1.00</div>
+              <div>Right:+1.00</div>
+            </div>
+            <div className="col-lg-4">
+              <div>IPD</div>
+              <div>Left: 32.0</div>
+              <div>Right: 32.0</div>
+            </div>
+            <div className="col-lg-4">
+              <div>Frame</div>
+              <div>Bridge: 22</div>
+              <div>Lens: 49</div>
+              <div>Temple: 150</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <div>Distance</div>
+              <div>Left:-1.00</div>
+            </div>
+            <div className="col-lg-4">
+              <div>Price</div>
+              <div>$9.99</div>
+            </div>
+            <div className="col-lg-4 mt-3">
+              <button>View</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
